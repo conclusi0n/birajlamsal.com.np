2e7aec3ac355701928936f93006099c3e6437e6f
Biraj Lamsal <95430391+conclusi0n@users.noreply.github.com>
2024-05-13 12:45:01 +0545

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,88 @@
+var youtube = "https://www.youtube.com/@conclu5ion";
+var twitter = "https://www.twitter.com/birajlamsal/";
+var password = "birajisgod";
+var linkedin = "https://www.linkedin.com/in/biraj-lamsal-873a71287/";
+var instagram = "https://www.instagram.com/birajlamsal/";
+var github = "https://github.com/conclusi0n/";
+var email = 'mailto:birajlamsal04@gmail.com';
+var facebook = "https://www.facebook.com/birajlamsalhoma/";
+var work ="https://www.youtube.com";
+var discord="https://discord.birajlamsal.com.np";
+
+var number="+977 9869086624"
+
+whois = [
+  "<br>",
+  "Hey, I'm Biraj!👋",
+  "I'm a high school student with a passion for learning new things, especially in the field of computer engineering.",
+  "I'm interested in exploring coding, software development, and computer science.",
+  "After finishing high school, I'm considering pursuing computer engineering as a career path.",
+  "While I haven't started making videos yet, I'm thinking about creating tutorials to share what I learn with others.",
+  "I'm excited about the prospect of documenting my journey and helping others navigate the world of technology.",
+  "Feel free to join me on this learning adventure!",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/Dimension' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/birajlamsal' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/birajlamsal' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/birajlamsal' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/birajlamsal' + "</a>",
+  'facebook       <a href="' + facebook +'" target="_blank">facebook/birajlamsalhoma'+"</a>",
+  'discord        <a href="' + discord + '" target="_blank">youtube/Dimension' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+ "<br>",
+ "Still curating... most projects are offline, on GitHub, or confidential.",
+"<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Biraj?',
+  //'<span class="command">whoami</span>         Who are you?',
+  //'<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           It is what it says',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Biraj Lamsal Is Not A Corporation. All Rights reserved.</span>',
+".----------------.  .----------------.  .----------------.  .----------------.  .----------------. ",
+"  | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",
+"  | |   ______     | || |     _____    | || |  _______     | || |      __      | || |     _____    | |",
+"  | |  |_   _ \    | || |    |_   _|   | || | |_   __ \    | || |     /  \     | || |    |_   _|   | |",
+"  | |    | |_) |   | || |      | |     | || |   | |__) |   | || |    / /\ \    | || |      | |     | |",
+"  | |    |  __'.   | || |      | |     | || |   |  __ /    | || |   / ____ \   | || |   _  | |     | |",
+"  | |   _| |__) |  | || |     _| |_    | || |  _| |  \ \_  | || | _/ /    \ \_ | || |  | |_' |     | |",
+"  | |  |_______/   | || |    |_____|   | || | |____| |___| | || ||____|  |____|| || |  `.___.'     | |",
+"  | |              | || |              | || |              | || |              | || |              | |",
+"  | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",
+"   '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ",
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
